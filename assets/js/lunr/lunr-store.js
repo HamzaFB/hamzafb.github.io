83c66e9f05cad3b38526bb6dab102c68a3b0ed6a
GitHub Action <action@github.com>
2022-09-06 10:49:09 +0000

    Build 326741f915

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Notes on Bayesian Online Change Point Detection",
-        "excerpt":"In this post I am going to explain the mathematical details behind the graphical model Bayesian Online Change Point Detection introduced in (Adams &amp; MacKay, 2007). This model can be used to detect different type of change-points and has known many extensions over the last few years. Given a time...","categories": [],
+        "excerpt":"In this post I am going to delve into the mathematical details behind the graphical model Bayesian Online Change Point Detection introduced in (Adams &amp; MacKay, 2007). This model can be used to detect different type of change-points and has known many extensions over the last few years. Given a...","categories": [],
         "tags": ["bayesian","change-point","online change-point"],
         "url": "/notes-on-bayesian-online-change-point-detection/",
-        "teaser": "/assets/images/500x300.png"
+        "teaser": null
       }]
