0ab2b5b122482af2643ab0eed3ecbc71583c82c2
GitHub Action <action@github.com>
2022-09-06 08:41:55 +0000

    Build f4cc4ecece

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "Notes on BOCPD",
+        "title": "Notes on Bayesian Online Change Point Detection",
         "excerpt":"Introduction In this post I am going to explain the graphical model Bayesian Online Change Point Detection introduced in (Adams &amp; MacKay, 2007) that has known many extensions over the last few years. Given a time series, we are interested in detecting structural changes as new data comes in 1....","categories": [],
         "tags": [],
         "url": "/notes-on-bayesian-online-change-point-detection/",
